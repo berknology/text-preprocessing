0728d7421be81a6d7d1aac0c7dcf0e1ec3279f80
He Hao <hehao@microsoft.com>
2020-05-13 14:30:07 -0700

    Bump version: 0.0.4 → 0.0.5

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ with open('README.rst') as description_file:
 
 setup(
     name='text_preprocessing',
-    version='0.0.4',
+    version='0.0.5',
     description="A python package for text preprocessing task in natural language processing",
     long_description=description,
     url='https://github.com/berknology/text-preprocessing',
